5530ea4b0c257706e95ba9a25f73c7eb7f9671c2
Victor Rojas <mator0296@hotmail.com>
2017-10-29 18:05:02 -0400

    start

--- Models/pedido.js
+++ Models/pedido.js
@@ -1,47 +1,18 @@
 
-
-
- var  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://localhost/SweetsTemptations',
-    mongoURLLabel = "";
-Object.assign=require('object-assign')
-//creacion del modelo json
-if (mongoURL == 'mongodb://localhost/SweetsTemptations' && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-
-  mongoURL = ""
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-
-
-
-
   var mongoose = require('mongoose');
-   console.log(mongoURL)
-  mongoose.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
 
- 
+var conection_string = 'mongodb://localhost/SweetsTemptations'
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  conection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + 
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + "/" + 
+  process.env.OPENSHIFT_APP_NAME ;
 
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
+
+}
+mongoose.connect( conection_string , { useMongoClient: true });
+//creacion del modelo json
 
 
 var pedidoSchemaJSON = {
